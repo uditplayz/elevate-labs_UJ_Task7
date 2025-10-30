2491aa065c20a370431ebf4d675b98bea84705e1
Udit Jain <77978984+uditplayz@users.noreply.github.com>
2025-10-30 23:44:54 +0530

    Modify startup script for Apache installation and HTML
    Updated the HTML content and ensured Apache is installed before starting the service.

--- startup-user-script.sh
+++ startup-user-script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 apt update -y
-apt install -y apache2
-echo "<h2>Hello from instance!</h2>" > /var/www/html/index.html
+apt install apache2 -y
 systemctl start apache2
-systemctl enable apache2
+echo "<h1>Hello from $(hostname)</h1><p>Welcome to my web page that has load balancing</p>" > /var/www/html/index.html
